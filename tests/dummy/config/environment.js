d33d486dee73f4e2824609c79c7e2a5b36822a76
David Pett <dpett@folklore.digital>
2016-08-25 10:37:37 -0500

    gh-pages fix

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -40,8 +40,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.rootUrl = '/ember-eline/';
-
+    ENV.rootURL = '/ember-eline/';
   }
 
   return ENV;
